9b2c46b7b6dca944b58430a3584394316451efc5
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2024-02-28 20:59:25 +0900

    commit

--- src/_assets/global/index.js
+++ src/_assets/global/index.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import store from '../_store/index.js';
 
-import Header from "../_components/Header.vue";
-import Footer from "../_components/Footer.vue";
+import Header from "../_components/common/Header.vue";
+import Footer from "../_components/common/Footer.vue";
 
 createApp(Header).use(store).mount('#header');
 createApp(Footer).use(store).mount('#footer');
--- src/_assets/pages/index/index.js
+++ src/_assets/pages/index/index.js
@@ -1,10 +1,12 @@
 import { createApp, defineComponent } from 'vue';
 import store from '../../_store/index.js';
 
+import Test from '../../_components/Test.vue';
+
 createApp(defineComponent({
   name: 'Contents',
   components: {
-
+    Test
   }
 }))
 .use(store)
