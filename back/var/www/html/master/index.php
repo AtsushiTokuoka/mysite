c4ad6acfb100d9be78ba5c1f7c579ef7d9e06017
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2024-05-21 15:08:14 +0900

    commit

--- back/var/www/html/master/index.php
+++ back/var/www/html/master/index.php
@@ -1,11 +1,25 @@
+<?php
+  require '/var/www/html/vendor/autoload.php';
+  use Vimeo\Vimeo;
+  $client = new Vimeo(
+    getenv('VIMEO_CLIENT_ID'), 
+    getenv('VIMEO_CLIENT_SECRET'), 
+    getenv('VIMEO_ACCESS_TOKEN')
+  );
+  $response = $client->request('/tutorial', array(), 'GET');
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="<?php echo getenv('CDN_URL'); ?>/favicon.svg" type="image/svg+xml" sizes="any">
   <title>Document</title>
 </head>
 <body>
   <h1>master/index.php</h1>
+  <pre><?php var_dump($client);?></pre>
+  <pre><?php var_dump($response);?></pre>
 </body>
 </html>
