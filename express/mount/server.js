0467893abb354c6fc62304a69d148d28a3b6f133
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2024-09-29 22:57:34 +0900

    設定ファイル変更

--- express/mount/server.js
+++ express/mount/server.js
@@ -16,7 +16,7 @@ server.use(express.static(path.join(__dirname, "public")));
 const validate = (access_key, token) => {
   // 開発環境用のアクセス制御
   if (process.env.APP_ENV === "local") {
-    if (access_key === process.env.AUTH_DEV_ACCESS_KEY) {
+    if (access_key === process.env.ADMIN_DEV_ACCESS_KEY) {
       return 200;
     } else {
       return checkToken(token);
@@ -29,10 +29,10 @@ const validate = (access_key, token) => {
   // tokenによる認証
   function checkToken(token) {
     if (token === undefined) return 401;
-    const verifyToken = JWT.verify(token, process.env.AUTH_SECRET_KEY);
+    const verifyToken = JWT.verify(token, process.env.ADMIN_SECRET_KEY);
     if (
-      process.env.AUTH_USER === verifyToken.username &&
-      process.env.AUTH_PASSWORD === verifyToken.password
+      process.env.ADMIN_USER === verifyToken.username &&
+      process.env.ADMIN_PASSWORD === verifyToken.password
     ) {
       return 200;
     } else {
@@ -43,14 +43,14 @@ const validate = (access_key, token) => {
 };
 
 server.get("/", (req, res) => {
-  const access_key = req.headers["x-auth-access-key"];
+  const access_key = req.headers["x-admin-access-key"];
   const token = req.cookies.auth_token;
   const status = validate(access_key, token);
   res.status(status).send();
 });
 
 server.get("/token", (req, res) => {
-  const access_key = req.headers["x-auth-access-key"];
+  const access_key = req.headers["x-admin-access-key"];
   const token = req.query.token;
   const status = validate(access_key, token);
   res.status(status).send();
@@ -58,16 +58,16 @@ server.get("/token", (req, res) => {
 
 server.post("/login", (req, res) => {
   if (
-    req.body.username === process.env.AUTH_USER &&
-    req.body.password === process.env.AUTH_PASSWORD
+    req.body.username === process.env.ADMIN_USER &&
+    req.body.password === process.env.ADMIN_PASSWORD
   ) {
     const redirectPath = req.body.redirectPath || "/";
     const token = JWT.sign(
       {
-        username: process.env.AUTH_USER,
-        password: process.env.AUTH_PASSWORD,
+        username: process.env.ADMIN_USER,
+        password: process.env.ADMIN_PASSWORD,
       },
-      process.env.AUTH_SECRET_KEY
+      process.env.ADMIN_SECRET_KEY
     );
     res
       .status(200)
